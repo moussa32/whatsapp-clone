582ea53eddbeb7e3e91b72ff341d0e5a0a4be1ec
Moussa Ibrahim <moussaebrahem32@gmail.com>
2020-09-13 07:25:49 +0200

    add production version

---
+++ build/precache-manifest.0c022382aea00ec5b8ad3457568d9daf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b49c73c9572f439e1929abdc107795d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c20515f8db3f0e53db8",
+    "url": "/static/css/main.b28a5aeb.chunk.css"
+  },
+  {
+    "revision": "7e86e52b8d05c46cfb48",
+    "url": "/static/js/2.afbab90b.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.afbab90b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c20515f8db3f0e53db8",
+    "url": "/static/js/main.0547f3c8.chunk.js"
+  },
+  {
+    "revision": "8ee40f9b318c3c875d87",
+    "url": "/static/js/runtime-main.67bfdc7e.js"
+  }
+]);
